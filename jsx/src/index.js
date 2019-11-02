d91c73fc11957ac24f00d5fc293c693d8d77556d
Waleed Ali <waleedali070@gmail.com>
2019-11-02 14:50:44 +0500

    added basic JSX example

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function getCurrentTime() {
+  return (new Date()).toLocaleTimeString()
+}
+const App = () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <h3>Welcome to My React Learning Project - Chapter JSX</h3>
+      
+      <p>
+        this chapter will only contain some basic project structure based on <code>create-react-app </code> 
+         and a very basic example of JSX rendering:
+      </p>
+      <ul>
+        <li>Class and Style Attributes</li>
+        <li>JS Variable Interpolation (displaying current time)</li>
+      </ul>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <div style={{ color: 'red' }}>Current Time: <b>{getCurrentTime()}</b></div>
+      <small>
+        Note that this time is not updating. reason being that this function is only called at the time
+        of rendering. The updation part will be done in state chapter (we will update state variable which will force re-rendering of the updated value).
+      </small>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
